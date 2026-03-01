2a2d5c6b4f9638cf3d57590731e405cb003dcd00
UENO, M. <eunosium-1128@live.jp>
2026-03-01 23:10:59 +0900

    Update linker flags to include rpath-link option

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,9 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_SHARED_LINKER_FLAGS="${CMAKE_SHARED_LINKER_FLAGS} -pthread" \
-      -DCMAKE_EXE_LINKER_FLAGS="${CMAKE_EXE_LINKER_FLAGS} -pthread" \
-      -DBUILD_SHARED_LIBS=ON \
-      -DPostgreSQL_ROOT=$PREFIX \
-      -DPostgreSQL_LIBRARY=$PREFIX/lib/libpq${SHLIB_EXT} \
-      -DPostgreSQL_INCLUDE_DIR=$PREFIX/include
+      -DCMAKE_SHARED_LINKER_FLAGS="${CMAKE_SHARED_LINKER_FLAGS} -pthread -Wl,-rpath-link,$PREFIX/lib" \
+      -DCMAKE_EXE_LINKER_FLAGS="${CMAKE_EXE_LINKER_FLAGS} -pthread -Wl,-rpath-link,$PREFIX/lib" \
+      -DBUILD_SHARED_LIBS=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
